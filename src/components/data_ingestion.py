18a075008428f70528a5340c49b98b52084f33f6
Darshan T <Darshandarsh.t1999@gmail.com>
2025-08-04 12:44:09 +0530

    Update data_ingestion and model_trainer; add model.pkl

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,15 +6,20 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
- 
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
     test_data_path: str = os.path.join('artifacts', "test.csv")
     raw_data_path: str = os.path.join('artifacts', "data.csv")
+
 
 class DataIngestion:
     def __init__(self):
@@ -34,8 +39,8 @@ class DataIngestion:
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True) 
-         
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
             logging.info("Ingestion of the data is completed")
 
             return (
@@ -45,10 +50,14 @@ class DataIngestion:
 
         except Exception as e:
             raise CustomException(e, sys)
-        
+
+
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
